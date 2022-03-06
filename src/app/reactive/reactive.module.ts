4319c6182e7d8259a088e1592c03c0db0530e374
EnocPAguirre <enocpinedaa@gmail.com>
2022-03-06 12:11:21 -0600

    Componentes reactivos

---
+++ src/app/reactive/reactive.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicosComponent } from './basicos/basicos.component';
+
+
+@NgModule({
+  declarations: [
+    BasicosComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveRoutingModule
+  ]
+})
+export class ReactiveModule { }
